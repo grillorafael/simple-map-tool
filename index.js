42e1d1e5425d9a1c865a0514d2d8c8cfa85998d1
Rafael Grillo <grillorafael@gmail.com>
2016-07-16 15:27:33 +0100

    Add vessel display

--- index.js
+++ index.js
@@ -12,17 +12,34 @@ let id = 0;
 // Attach a map to the DOM Element, with the defined settings
 const map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 const addBtn = document.getElementById('add-element-button');
+const addVesselBtn = document.getElementById('add-vessel-button');
+
 const addDialog = document.getElementById('add-dialog');
-const addForm = document.getElementById('add-form');
+const addVesselDialog = document.getElementById('add-vessel-dialog');
+
+const addElement = document.getElementById('add-form');
+const addVesselForm = document.getElementById('add-vessel-form');
+
 const textField = document.querySelector('textarea[name=coordinates]');
 const snackbarContainer = document.querySelector('#delete-snackbar');
 
 addBtn.addEventListener('click', openAddDialog);
-addForm.addEventListener('submit', formSubmit);
-addDialog.querySelector('.close').addEventListener('click', () => addDialog.close());
+addVesselBtn.addEventListener('click', openVesselAddDialog);
+addElement.addEventListener('submit', formElementSubmit);
 
-function openAddDialog() { addDialog.showModal(); }
-function formSubmit(e) {
+addVesselForm.addEventListener('submit', addVesselSubmit);
+addDialog.querySelector('.close').addEventListener('click', () => addDialog.close());
+addVesselDialog.querySelector('.close').addEventListener('click', () => addVesselDialog.close());
+
+function openVesselAddDialog() {
+    addVesselDialog.showModal();
+}
+
+function openAddDialog() {
+    addDialog.showModal();
+}
+
+function formElementSubmit(e) {
     e.preventDefault();
     addDialog.close();
 
@@ -31,14 +48,13 @@ function formSubmit(e) {
     let positions = parseInput(content);
     let newElement;
 
-    if(positions.length === 1) {
+    if (positions.length === 1) {
         newElement = new google.maps.Marker({
             id: elementId,
             position: positions[0],
             map: map
         });
-    }
-    else if(positions.length > 1) {
+    } else if (positions.length > 1) {
         newElement = new google.maps.Polygon({
             id: elementId,
             paths: positions,
@@ -71,6 +87,112 @@ function checkDeletion(e) {
     snackbarContainer.MaterialSnackbar.showSnackbar(data);
 }
 
+function angleAsHeading(a) {
+    if (a >= 360) {
+        return a % 360
+    } else if (a < 0) {
+        return 360 + a
+    } else return a
+}
+
+function toRad(a) {
+    return a * (Math.PI / 180);
+}
+
+function toDeg(a) {
+    return a * (180/Math.PI);
+}
+
+let GeoUtil = {
+    travel: function(pos, heading, distance) {
+        return geolib.computeDestinationPoint({latitude: pos.lat(), longitude: pos.lng()}, distance, heading)
+    }
+}
+
+function addVesselSubmit(e) {
+    e.preventDefault();
+    addVesselDialog.close();
+    let data = {
+        antennaCoordinates: document.querySelector('#vessel-coordinates').value.split(',').map(_ => _.trim()).map(parseFloat),
+        heading: parseFloat(document.querySelector('#vessel-heading').value.trim()),
+        width: parseFloat(document.querySelector('#vessel-width').value.trim()),
+        length: parseFloat(document.querySelector('#vessel-length').value.trim()),
+        dims: document.querySelector('#vessel-dims').value.split(',').map(_ => _.trim()).map(parseFloat),
+    }
+
+    map.setCenter(toupleAsLatLng(data.antennaCoordinates));
+    map.setZoom(18);
+
+    let antennaPosition = toupleAsLatLng(data.antennaCoordinates)
+
+    // offsets of antenna position
+    let offsetLeft = data.dims[2]
+    let offsetBottom = data.dims[1]
+    let offsetRight = data.dims[3]
+    let offsetTop = data.dims[0]
+
+    let heading = toRad(data.heading)
+
+    var aHeadingIncrement = Math.atan(offsetLeft / offsetBottom)
+    if(Number.isNaN(aHeadingIncrement)) aHeadingIncrement = Math.PI/2
+
+    var bHeadingIncrement = Math.atan(offsetRight / offsetBottom)
+    if(Number.isNaN(bHeadingIncrement)) bHeadingIncrement = Math.PI/2
+
+    var dHeadingIncrement = Math.atan(offsetLeft / offsetTop)
+    if(Number.isNaN(dHeadingIncrement)) dHeadingIncrement = Math.PI/2
+
+    var cHeadingIncrement = Math.atan(offsetRight / offsetTop)
+    if(Number.isNaN(cHeadingIncrement)) cHeadingIncrement = Math.PI/2
+
+    // left bottom
+    let aHeading = angleAsHeading(toDeg(Math.PI + heading + aHeadingIncrement))
+    let aDistance = Math.sqrt(Math.pow(offsetLeft, 2) + Math.pow(offsetBottom, 2))
+    let a = GeoUtil.travel(antennaPosition, aHeading, aDistance)
+
+    // right bottom
+    let bHeading = angleAsHeading(toDeg(Math.PI + heading - bHeadingIncrement))
+    let bDistance = Math.sqrt(Math.pow(offsetRight, 2) + Math.pow(offsetBottom, 2))
+    let b = GeoUtil.travel(antennaPosition, bHeading, bDistance)
+
+    // top right
+    let cHeading = angleAsHeading(toDeg(heading + cHeadingIncrement))
+    let cDistance = Math.sqrt(Math.pow(offsetRight, 2) + Math.pow(offsetTop, 2))
+    let c = GeoUtil.travel(antennaPosition, cHeading, cDistance)
+
+    //top left
+    let dHeading = angleAsHeading(toDeg(heading - dHeadingIncrement))
+    let dDistance = Math.sqrt(Math.pow(offsetLeft, 2) + Math.pow(offsetTop, 2))
+    let d = GeoUtil.travel(antennaPosition, dHeading, dDistance)
+
+    let pos = [a, b, c, d].map(p => toupleAsLatLng([p.latitude, p.longitude]))
+
+    let antennaId = ++id;
+    let elementId = ++id;
+
+    let polygon = new google.maps.Polygon({
+        id: elementId,
+        paths: pos,
+        map: map,
+        strokeColor: 'black',
+        strokeOpacity: 1,
+        fillColor: 'black',
+        fillOpacity: 0.4
+    });
+
+    let antenna = new google.maps.Marker({
+        id: antennaId,
+        position: antennaPosition,
+        map: map
+    });
+
+    polygon.addListener('click', checkDeletion);
+    mapElements.set(elementId, polygon);
+
+    antenna.addListener('click', checkDeletion);
+    mapElements.set(antennaId, antenna);
+}
+
 /**
  * Returns list of LatLng
  */
@@ -78,7 +200,11 @@ function parseInput(inText) {
     const coords = inText.split("\n");
     return coords.map(_ => {
         let latLng = _.split(',');
-        if(!_) return undefined;
-        return new google.maps.LatLng(latLng[0], latLng[1]);
+        if (!_) return undefined;
+        return toupleAsLatLng(latLng);
     }).filter(_ => _ !== undefined);
+}
+
+function toupleAsLatLng(input) {
+    return new google.maps.LatLng(input[0], input[1])
 }
